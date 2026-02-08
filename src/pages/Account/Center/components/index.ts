4f7ab0f76444e3cee002f77bfa144af3eab32184
StephenQiu30 <Popcornqhd@gmail.com>
2026-02-08 20:34:43 +0800

    refactor: :recycle: 去掉一些不必要的页面

--- src/pages/Account/Center/components/index.ts
+++
@@ -1,3 +0,0 @@
-import { MyFavourPostList, MyPostList, MyThumbPostList } from '@/pages/Account/Center/components/post';
-
-export { MyPostList, MyFavourPostList, MyThumbPostList };
--- src/pages/Account/Center/components/post/index.ts
+++
@@ -1,5 +0,0 @@
-import MyPostList from '@/pages/Account/Center/components/post/MyPostList';
-import MyFavourPostList from '@/pages/Account/Center/components/post/MyFavourPostList';
-import MyThumbPostList from '@/pages/Account/Center/components/post/MyThumbPostList';
-
-export { MyPostList, MyFavourPostList, MyThumbPostList };
